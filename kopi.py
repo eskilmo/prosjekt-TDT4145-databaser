e29fbca561c09a2fc5fc78c79caf5d12157393c1
Håkon <99hakon@hotmail.no>
2023-03-22 17:13:01 +0100

    funksjon

---
+++ kopi.py
@@ -0,0 +1,59 @@
+import sqlite3 as sq
+
+def buy_tickets():
+    con = sq.connect("prosjekt.db")
+    cursor = con.cursor()
+    print("Login for å få kjøpt billetter:")
+    navn = input("Navn: ")
+    epost = input("Epost: ")
+    if valid_customer(navn, epost) == False:
+            raise Exception("Du har ikke registrert deg i kunderegisteret.")
+
+    #LEGG INN KODE HER HÅKON
+
+    #få metode for å få inn alle e finne ledige billetter for en oppgitt strekning 
+    #på en ønsket togrute og kjøpe de billettene hen ønsker
+    #ledigeBilletter=
+    
+    cursor.execute("SELECT kundNR FROM Kunde WHERE navn = ? AND epost = ?", (navn, epost))
+    kundeNR = cursor.fetchone()[0]
+    cursor.execute("SELECT * FROM Kundeordre")
+    rows = cursor.fetchall()
+    if rows == None:
+        ordreNR = 1
+    else:
+        ordreNR = len(rows) + 1
+    
+    cursor.execute('''INSERT INTO Bestilling VALUES (?, ?)''', (kundeNR, ordreNR))
+    antallBilletter = input("Hvor mange billetter vil du kjøpe?")
+    if AntallBilletter > ledigeBilletter:
+        raise Exception("Det er ikke så mange billetter som er tilgjengelig på denne delstrekningen.")
+    bestillingsdato = date.today().strftime("%m/%d/%Y")
+    #bestillingstid = 
+
+    
+con = sq.connect("prosjekt.db")
+cursor = con.cursor()
+
+dato = input("Hvilken dato vil du reise? ")
+startstasjon = input("Hvor reiser du fra? ")
+sluttstasjon = input("Hvor vil du reise til? ")
+plass = input("Seng eller sete? ")
+
+cursor.execute()
+
+if plass.lower() == "seng":
+    cursor.execute('''SELECT * 
+                    FROM SengLedigPåTogreise INNER JOIN Togreise
+                    on SengLedigPåTogreise.togreiseID = Togreise.togreiseID
+                    WHERE (Togreise.dato = ? AND ledig = 1)''', (dato,))
+    rows = cursor.fetchall()
+    print(rows)
+
+else:
+    cursor.execute('''SELECT * 
+                    FROM SeteLedigPåDelstrekning INNER JOIN Togreise
+                    on SeteLedigPåDelstrekning.togreiseID = Togreise.togreiseID
+                    WHERE (Togreise.dato = ? AND ledig = 1)''', (dato,))
+    rows = cursor.fetchall()
+    print(rows)
