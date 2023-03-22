88015927db15a8a3f4c5f555bf796740b671fcb3
Håkon <99hakon@hotmail.no>
2023-03-22 09:43:06 +0100

    noe

---
+++ init.sql
@@ -0,0 +1,189 @@
+INSERT INTO Jernbanestasjon (navn, moh) VALUES ("Trondheim", 5.1);
+INSERT INTO Jernbanestasjon (navn, moh) VALUES ("Steinkjer", 3.6);
+INSERT INTO Jernbanestasjon (navn, moh) VALUES ("Mosjøen", 6.8);
+INSERT INTO Jernbanestasjon (navn, moh) VALUES ("Mo i Rana", 3.5);
+INSERT INTO Jernbanestasjon (navn, moh) VALUES ("Fauske", 34.0);
+INSERT INTO Jernbanestasjon (navn, moh) VALUES ("Bodø", 4.1);
+
+INSERT INTO Banestrekning (navn, startstasjon, endestasjon, fremdriftsenergi) VALUES ("Nordlandsbanen", "Trondheim", "Bodø", "Diesel");
+
+INSERT INTO Delstrekning (delstrekningsID, lengde, sportype, startstasjon, sluttstasjon) VALUES (1, 120, "Dobbel", "Trondheim", "Steinkjer");
+INSERT INTO Delstrekning (delstrekningsID, lengde, sportype, startstasjon, sluttstasjon) VALUES (2, 280, "Enkel", "Steinkjer", "Mosjøen");
+INSERT INTO Delstrekning (delstrekningsID, lengde, sportype, startstasjon, sluttstasjon) VALUES (3, 90, "Enkel", "Mosjøen", "Mo i Rana");
+INSERT INTO Delstrekning (delstrekningsID, lengde, sportype, startstasjon, sluttstasjon) VALUES (4, 170, "Enkel", "Mo i Rana", "Fauske");
+INSERT INTO Delstrekning (delstrekningsID, lengde, sportype, startstasjon, sluttstasjon) VALUES (5, 60, "Enkel", "Fauske", "Bodø");
+
+INSERT INTO BestårAv (banestrekningsnavn, delstrekningsID) VALUES ("Nordlandsbanen", 1);
+INSERT INTO BestårAv (banestrekningsnavn, delstrekningsID) VALUES ("Nordlandsbanen", 2);
+INSERT INTO BestårAv (banestrekningsnavn, delstrekningsID) VALUES ("Nordlandsbanen", 3);
+INSERT INTO BestårAv (banestrekningsnavn, delstrekningsID) VALUES ("Nordlandsbanen", 4);
+INSERT INTO BestårAv (banestrekningsnavn, delstrekningsID) VALUES ("Nordlandsbanen", 5);
+
+INSERT INTO operatør (antallVogner, navn) VALUES (5, "SJ");
+
+INSERT INTO Togrute (ruteID, startstasjon, endestasjon, hovedretning, operatør) VALUES (1, "Trondheim", "Bodø", "med", "SJ");
+INSERT INTO Togrute VALUES (2, "Trondheim", "Bodø", "med", "SJ");
+INSERT INTO Togrute VALUES (3, "Mo i Rana", "Trondheim", "mot", "SJ");
+
+INSERT INTO Togreise VALUES (1, 1, "03.04.2023");
+INSERT INTO Togreise VALUES (2, 2, "03.04.2023");
+INSERT INTO Togreise VALUES (3, 3, "03.04.2023");
+
+INSERT INTO Togreise VALUES (4, 1, "04.04.2023");
+INSERT INTO Togreise VALUES (5, 2, "04.04.2023");
+INSERT INTO Togreise VALUES (6, 3, "04.04.2023");
+
+INSERT INTO Vogn (vognID, vognNR, navn) VALUES (1, 1,"SJ-sittevogn1");
+INSERT INTO Vogn (vognID, vognNR, navn) VALUES (3, 2,"SJ-sittevogn1");
+INSERT INTO Vogn (vognID, vognNR, navn) VALUES (4, 1,"SJ-sittevogn1");
+INSERT INTO Vogn (vognID, vognNR, navn) VALUES (2, 2,"SJ-sovevogn1");
+INSERT INTO Vogn (vognID, vognNR, navn) VALUES (5, 1,"SJ-sittevogn1");
+
+INSERT INTO Sittevogn (vognID, antallStolrader, antallSeterPrRad) VALUES (1, 3, 4);
+INSERT INTO Sovevogn (vognID, antallSovekupeer) VALUES (2, 4);
+INSERT INTO Sittevogn (vognID, antallStolrader, antallSeterPrRad) VALUES (3, 3, 4);                             
+INSERT INTO Sittevogn (vognID, antallStolrader, antallSeterPrRad) VALUES (4, 3, 4);                    
+INSERT INTO Sittevogn (vognID, antallStolrader, antallSeterPrRad) VALUES (5, 3, 4);
+
+INSERT INTO Avgangsdager VALUES (1, "mandag");
+INSERT INTO Avgangsdager VALUES (1, "tirsdag");
+INSERT INTO Avgangsdager VALUES (1, "onsdag");
+INSERT INTO Avgangsdager VALUES (1, "torsdag");
+INSERT INTO Avgangsdager VALUES (1, "fredag");
+
+INSERT INTO Avgangsdager VALUES (2, "mandag");
+INSERT INTO Avgangsdager VALUES (2, "tirsdag");
+INSERT INTO Avgangsdager VALUES (2, "onsdag");
+INSERT INTO Avgangsdager VALUES (2, "torsdag");
+INSERT INTO Avgangsdager VALUES (2, "fredag");
+INSERT INTO Avgangsdager VALUES (2, "lørdag");
+INSERT INTO Avgangsdager VALUES (2, "søndag");
+
+INSERT INTO Avgangsdager VALUES (3, "mandag");
+INSERT INTO Avgangsdager VALUES (3, "tirsdag");
+INSERT INTO Avgangsdager VALUES (3, "onsdag");
+INSERT INTO Avgangsdager VALUES (3, "torsdag");
+INSERT INTO Avgangsdager VALUES (3, "fredag");
+
+INSERT INTO Ruter (togruteID, banestrekningsnavn) VALUES (1, "Nordlandsbanen");
+INSERT INTO Ruter (togruteID, banestrekningsnavn) VALUES (2, "Nordlandsbanen");
+INSERT INTO Ruter (togruteID, banestrekningsnavn) VALUES (3, "Nordlandsbanen");
+
+INSERT INTO Vognoppsett (ruteID, vognID) VALUES (1, 1); 
+INSERT INTO Vognoppsett (ruteID, vognID) VALUES (1, 3);
+INSERT INTO Vognoppsett (ruteID, vognID) VALUES (2, 4);
+INSERT INTO Vognoppsett (ruteID, vognID) VALUES (2, 2);  
+INSERT INTO Vognoppsett (ruteID, vognID) VALUES (3, 5);
+
+INSERT INTO Seng (sengNR) VALUES (1);
+INSERT INTO Seng (sengNR) VALUES (2);
+INSERT INTO Seng (sengNR) VALUES (3);
+INSERT INTO Seng (sengNR) VALUES (4);
+INSERT INTO Seng (sengNR) VALUES (5);
+INSERT INTO Seng (sengNR) VALUES (6);
+INSERT INTO Seng (sengNR) VALUES (7);
+INSERT INTO Seng (sengNR) VALUES (8);
+
+INSERT INTO SengIVogn (vognID, sengNR) VALUES (2, 1);
+INSERT INTO SengIVogn (vognID, sengNR) VALUES (2, 2);
+INSERT INTO SengIVogn (vognID, sengNR) VALUES (2, 3);
+INSERT INTO SengIVogn (vognID, sengNR) VALUES (2, 4);
+INSERT INTO SengIVogn (vognID, sengNR) VALUES (2, 5);
+INSERT INTO SengIVogn (vognID, sengNR) VALUES (2, 6);
+INSERT INTO SengIVogn (vognID, sengNR) VALUES (2, 7);
+INSERT INTO SengIVogn (vognID, sengNR) VALUES (2, 8);
+
+INSERT INTO Togrutetabell VALUES (1, "Trondheim", NULL, "07:49");
+INSERT INTO Togrutetabell VALUES (1, "Steinkjer", NULL, "09:51");
+INSERT INTO Togrutetabell VALUES (1, "Mosjøen", NULL, "13:20");
+INSERT INTO Togrutetabell VALUES (1, "Mo i Rana", NULL, "14:31");
+INSERT INTO Togrutetabell VALUES (1, "Fauske", NULL, "16:49");
+INSERT INTO Togrutetabell VALUES (1, "Bodø", "17:34", NULL);
+
+INSERT INTO Togrutetabell VALUES (2, "Trondheim", NULL, "23:05");
+INSERT INTO Togrutetabell VALUES (2, "Steinkjer", NULL, "00:57");
+INSERT INTO Togrutetabell VALUES (2, "Mosjøen", NULL, "04:41");
+INSERT INTO Togrutetabell VALUES (2, "Mo i Rana", NULL, "05:55");
+INSERT INTO Togrutetabell VALUES (2, "Fauske", NULL, "08:19");
+INSERT INTO Togrutetabell VALUES (2, "Bodø", "09:05", NULL);
+
+INSERT INTO Togrutetabell VALUES (3, "Mo i Rana", NULL, "08:11");
+INSERT INTO Togrutetabell VALUES (3, "Mosjøen", NULL, "09:14");
+INSERT INTO Togrutetabell VALUES (3, "Steinkjer", NULL, "12:31");
+INSERT INTO Togrutetabell VALUES (3, "Trondheim", "14:13", NULL);
+
+INSERT INTO RuteOperatør VALUES (1, "SJ");
+INSERT INTO RuteOperatør VALUES (2, "SJ");
+INSERT INTO RuteOperatør VALUES (3, "SJ");
+
+INSERT INTO Sete (seteNR) VALUES (1);
+INSERT INTO Sete (seteNR) VALUES (2);
+INSERT INTO Sete (seteNR) VALUES (3);
+INSERT INTO Sete (seteNR) VALUES (4);
+INSERT INTO Sete (seteNR) VALUES (5);
+INSERT INTO Sete (seteNR) VALUES (6);
+INSERT INTO Sete (seteNR) VALUES (7);
+INSERT INTO Sete (seteNR) VALUES (8);
+INSERT INTO Sete (seteNR) VALUES (9);
+INSERT INTO Sete (seteNR) VALUES (10);
+INSERT INTO Sete (seteNR) VALUES (11);
+INSERT INTO Sete (seteNR) VALUES (12);
+
+
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (1,1);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (1,2);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (1,3);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (1,4);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (1,5);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (1,6);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (1,7);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (1,8);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (1,9);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (1,10);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (1,11);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (1,12);
+
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (3,1);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (3,2);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (3,3);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (3,4);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (3,5);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (3,6);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (3,7);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (3,8);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (3,9);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (3,10);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (3,11);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (3,12);
+
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (4,1);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (4,2);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (4,3);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (4,4);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (4,5);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (4,6);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (4,7);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (4,8);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (4,9);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (4,10);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (4,11);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (4,12);
+
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (5,1);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (5,2);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (5,3);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (5,4);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (5,5);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (5,6);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (5,7);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (5,8);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (5,9);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (5,10);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (5,11);
+INSERT INTO SeteIVogn (vognID,seteNR) VALUES (5,12);
+
+INSERT INTO VognerTilgjengelig (vognID, operatørNavn) VALUES (1,"SJ");
+INSERT INTO VognerTilgjengelig (vognID, operatørNavn) VALUES (2,"SJ");
+INSERT INTO VognerTilgjengelig (vognID, operatørNavn) VALUES (3,"SJ");
+INSERT INTO VognerTilgjengelig (vognID, operatørNavn) VALUES (4,"SJ");
+INSERT INTO VognerTilgjengelig (vognID, operatørNavn) VALUES (5,"SJ");
---
+++ prosjektTDT4145_GR103.sql
@@ -0,0 +1,228 @@
+CREATE table Jernbanestasjon (
+    navn VARCHAR(40) NOT NULL,
+    moh FLOAT,
+    CONSTRAINT jernbanestrekning_PK PRIMARY KEY (navn));
+
+CREATE table Banestrekning (
+    navn VARCHAR(40) NOT NULL,
+    startstasjon VARCHAR(40) NOT NULL,
+    endestasjon VARCHAR(40) NOT NULL,
+    fremdriftsenergi VARCHAR(20),
+    CONSTRAINT banestrekning_PK PRIMARY KEY (navn)
+    CONSTRAINT banestrekning_FK1 FOREIGN KEY (startstasjon) REFERENCES Jernbanestasjon(navn) ON UPDATE CASCADE ON DELETE NO ACTION, 
+    CONSTRAINT banestrekning_FK2 FOREIGN KEY (endestasjon) REFERENCES Jernbanestasjon(navn) ON UPDATE CASCADE ON DELETE NO ACTION);
+
+CREATE table Delstrekning (
+    delstrekningsID INTEGER NOT NULL,
+    lengde INTEGER,
+    sportype VARCHAR(20),
+    startstasjon VARCHAR(40) NOT NULL,
+    sluttstasjon VARCHAR(40) NOT NULL,
+    CONSTRAINT delstrekning_PK PRIMARY KEY (delstrekningsID),
+    CONSTRAINT delstrekning_FK1 FOREIGN KEY (startstasjon) REFERENCES Jernbanestasjon(navn) ON UPDATE CASCADE ON DELETE NO ACTION, 
+    CONSTRAINT delstrekning_FK2 FOREIGN KEY (sluttstasjon) REFERENCES Jernbanestasjon(navn) ON UPDATE CASCADE ON DELETE NO ACTION);
+
+CREATE table BestårAv (
+    banestrekningsnavn VARCHAR(40) NOT NULL,
+    delstrekningsID INTEGER NOT NULL,
+    CONSTRAINT bestårav_PK PRIMARY KEY (banestrekningsnavn, delstrekningsID), 
+    CONSTRAINT bestårav_FK1 FOREIGN KEY (banestrekningsnavn) REFERENCES Banestrekning(navn) ON UPDATE CASCADE ON DELETE NO ACTION, 
+    CONSTRAINT bestårav_FK2 FOREIGN KEY (delstrekningsID) REFERENCES Delstrekning(delstrekningsID) ON UPDATE CASCADE ON DELETE NO ACTION);
+
+CREATE table Togrute (
+    ruteID INTEGER NOT NULL,
+    startstasjon VARCHAR(40) NOT NULL,
+    endestasjon VARCHAR(40) NOT NULL,
+    hovedretning VARCHAR(20),
+    operatør VARCHAR(40) NOT NULL,
+    CONSTRAINT togrute_PK PRIMARY KEY (ruteID),
+    CONSTRAINT togrute_FK1 FOREIGN KEY (startstasjon) REFERENCES Jernbanestasjon(navn) ON UPDATE CASCADE ON DELETE NO ACTION, 
+    CONSTRAINT togrute_FK2 FOREIGN KEY (endestasjon) REFERENCES Jernbanestasjon(navn) ON UPDATE CASCADE ON DELETE NO ACTION,
+    CONSTRAINT togrute_FK3 FOREIGN KEY (operatør) REFERENCES Operatør(navn) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE table Togreise (
+	togreiseID INTEGER NOT NULL,
+    togruteID INTEGER NOT NULL, 
+    dato VARCHAR(10) NOT NULL,
+	CONSTRAINT togreise_PK PRIMARY KEY (togreiseID),
+    CONSTRAINT togreise_FK1 FOREIGN KEY (togruteID) REFERENCES Togrute(ruteID) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE table Avgangsdager (
+    ruteID INTEGER NOT NULL,
+    dag VARCHAR(20) NOT NULL,
+    CONSTRAINT avgangsdager_PK PRIMARY KEY (ruteID, dag),
+	CONSTRAINT avgangsdager_FK1 FOREIGN KEY (ruteID) REFERENCES Togrute(ruteID) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE table Ruter (
+    togruteID INTEGER NOT NULL,
+    banestrekningsnavn VARCHAR(40) NOT NULL,
+    CONSTRAINT ruter_PK PRIMARY KEY (togruteID),
+    CONSTRAINT ruter_FK FOREIGN KEY (banestrekningsnavn) REFERENCES Banestrekning(navn) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE table Togrutetabell (
+    togruteID INTEGER NOT NULL,
+    jernbanestasjonsnavn VARCHAR(40) NOT NULL,
+    ankomsttid VARCHAR(10),
+    avgangstid VARCHAR(10),
+    CONSTRAINT togrutetabell_PK PRIMARY KEY (togruteID, jernbanestasjonsnavn),
+    CONSTRAINT togrutetabell_FK1 FOREIGN KEY (togruteID) REFERENCES Togrute(ruteID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT togrutetabell_FK2 FOREIGN KEY (jernbanestasjonsnavn) REFERENCES Jernbanestasjon(navn) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE table Vogn (
+    vognID INTEGER NOT NULL,
+    vognNR INTEGER,
+    navn VARCHAR(40),
+    CONSTRAINT vogn_PK PRIMARY KEY (vognID));
+
+CREATE table Vognoppsett (
+    vognID INTEGER NOT NULL,
+    ruteID INTEGER NOT NULL,
+    CONSTRAINT vognoppsett_PK PRIMARY KEY (vognID),
+	CONSTRAINT vognoppsett_FK1 FOREIGN KEY (vognID) REFERENCES Vogn(vognID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT vognoppsett_FK2 FOREIGN KEY (ruteID) REFERENCES Togrute(ruteID) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE table Sittevogn (
+    vognID INTEGER NOT NULL,
+    antallStolrader INTEGER,
+    antallSeterPrRad INTEGER,
+    CONSTRAINT sittevogn_PK PRIMARY KEY (vognID),
+	CONSTRAINT sittevogn_FK1 FOREIGN KEY (vognID) REFERENCES Vogn(vognID) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE table Sovevogn (
+    vognID INTEGER NOT NULL,
+    antallSovekupeer INTEGER,
+    CONSTRAINT sovevogn_PK PRIMARY KEY (vognID),
+	CONSTRAINT sovevogn_FK1 FOREIGN KEY (vognID) REFERENCES Vogn(vognID) ON UPDATE CASCADE ON DELETE CASCADE);
+	
+CREATE table Sete (
+	seteNR INTEGER NOT NULL,
+	CONSTRAINT sete_PK PRIMARY KEY (seteNR));
+	
+CREATE table SeteIVogn (
+	vognID INTEGER NOT NULL,
+	seteNR INTEGER NOT NULL,
+	CONSTRAINT seteivogn_PK PRIMARY KEY (vognID, seteNR),
+	CONSTRAINT seteivogn_FK1 FOREIGN KEY (vognID) REFERENCES Sittevogn(vognID) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT seteivogn_FK2 FOREIGN KEY (seteNR) REFERENCES Sete(seteNR) ON UPDATE CASCADE ON DELETE NO ACTION);
+	
+CREATE table SengIVogn (
+	vognID INTEGER NOT NULL,
+    sengNR INTEGER NOT Null,
+	CONSTRAINT sengivogn_PK PRIMARY KEY (vognID, sengNR),
+	CONSTRAINT sengivogn_FK1 FOREIGN KEY (vognID) REFERENCES Sovevogn(VognID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT sengivogn_FK2 FOREIGN KEY (sengNR) REFERENCES Seng(sengNR) ON UPDATE CASCADE ON DELETE NO ACTION);
+
+CREATE table Seng (
+	sengNR INTEGER NOT NULL,
+	CONSTRAINT seng_PK PRIMARY KEY (sengNR));
+
+CREATE table VognerTilgjengelig (
+	vognID INTEGER NOT NULL,
+	operatørNavn VARCHAR(40) NOT NULL,
+	CONSTRAINT vognertilgjengelig_PK PRIMARY KEY (vognID),
+	CONSTRAINT vognertilgjengelig_FK1 FOREIGN KEY (vognID) REFERENCES Vogn(vognID) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT vognertilgjengelig_FK2 FOREIGN KEY (operatørNavn) REFERENCES Operatør(navn) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE table Operatør (
+	antallVogner INTEGER,
+	navn VARCHAR(40) NOT NULL,
+	CONSTRAINT operatør_PK PRIMARY KEY (navn));
+
+CREATE table RuteOperatør (
+	ruteID INTEGER NOT NULL,
+	operatørNavn VARCHAR(40) NOT NULL,
+	CONSTRAINT ruteoperatør_PK PRIMARY KEY (ruteID),
+	CONSTRAINT ruteoperatør_FK1 FOREIGN KEY (operatørNavn) REFERENCES Operatør(navn) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT ruteoperatør_FK2 FOREIGN KEY (ruteID) REFERENCES Togrute(ruteID) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE table Kunde (
+	kundeNR INTEGER NOT NULL,
+	mobilNR INTEGER NOT NULL,
+    epost VARCHAR(50),
+    navn VARCHAR(40),
+	CONSTRAINT kunde_PK PRIMARY KEY (kundeNR));
+
+CREATE table Bestilling (
+	kundeNR INTEGER NOT NULL,
+	ordreNR INTEGER NOT NULL,
+	CONSTRAINT bestilling_PK PRIMARY KEY (kundeNR, ordreNR),
+    CONSTRAINT bestilling_FK1 FOREIGN KEY (kundeNR) REFERENCES Kunde(kundeNR) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT bestilling_FK2 FOREIGN KEY (ordreNR) REFERENCES Kundeordre(ordreNR) ON UPDATE CASCADE ON DELETE CASCADE);
+
+
+CREATE table Kundeordre (
+	antallBilletter INTEGER NOT NULL,
+	ordreNR INTEGER NOT NULL,
+    bestillingsDato VARCHAR(10) NOT NULL,
+    bestillingsTid VARCHAR(10) NOT NULL,
+	CONSTRAINT kundeordre_PK PRIMARY KEY (ordreNR));
+
+
+CREATE table BillettKjøp (
+    billettID INTEGER NOT NULL,
+    ordreNR INTEGER NOT NULL,
+    CONSTRAINT billett_PK PRIMARY KEY (billettID),
+    CONSTRAINT billett_FK1 FOREIGN KEY (billettID) REFERENCES Billett(billettID) ON UPDATE NO ACTION ON DELETE CASCADE, 
+    CONSTRAINT billett_FK2 FOREIGN KEY (ordreNR) REFERENCES Kundeordre(ordreNR) ON UPDATE NO ACTION ON DELETE CASCADE);
+
+CREATE table Billett (
+    billettID INTEGER NOT NULL,
+    startstasjon VARCHAR(40) NOT NULL,
+    endestasjon VARCHAR(40) NOT NULL,
+    avgangsdato VARCHAR(10) NOT NULL,
+    CONSTRAINT billett_PK PRIMARY KEY (billettID),
+    CONSTRAINT billett_FK1 FOREIGN KEY (startstasjon) REFERENCES Jernbanestasjon(navn) ON UPDATE CASCADE ON DELETE CASCADE, 
+    CONSTRAINT billett_FK2 FOREIGN KEY (endestasjon) REFERENCES Jernbanestasjon(navn) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE table ReservertSengeplass (
+    billettID INTEGER NOT NULL,
+    sengNR INTEGER NOT NULL,
+    vognID INTEGER NOT NULL,
+    CONSTRAINT reservertsengeplass_PK PRIMARY KEY (billettID),
+    CONSTRAINT reservertseteplass_FK1 FOREIGN KEY (billettID) REFERENCES Billett(billettID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT reservertseteplass_FK2 FOREIGN KEY (sengNR, vognID) REFERENCES SengIVogn(sengNR, vognID) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE table ReservertSetePlass (
+    billettID INTEGER NOT NULL,
+    seteNR INTEGER NOT NULL,
+    vognID INTEGER NOT NULL,
+    CONSTRAINT reservertseteplass_PK PRIMARY KEY (billettID),
+    CONSTRAINT reservertseteplass_FK1 FOREIGN KEY (billettID) REFERENCES Billett(billettID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT reservertseteplass_FK2 FOREIGN KEY (seteNR, vognID) REFERENCES SeteIVogn(seteNR, vognID) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE table SengLedigPåTogReise (
+	togreiseID INTEGER NOT NULL,
+    vognID INTEGER NOT NULL,
+    sengNR INTEGER NOT NULL,
+    ledig BOOLEAN,
+	CONSTRAINT sengledigpåtogreise_PK PRIMARY KEY (togreiseID, vognID, sengNR),
+    CONSTRAINT sengledigpåtogreise_FK1 FOREIGN KEY (togreiseID) REFERENCES Togreise(togreiseID) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT sengledigpåtogreise_FK2 FOREIGN KEY (vognID, sengNR) REFERENCES SengIVogn(vognID, sengNR) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE table SeteLedigPåDelstrekning (
+	togreiseID INTEGER NOT NULL,
+	delstrekningsID INTEGER NOT NULL,
+    vognID INTEGER NOT NULL,
+    seteNR INTEGER NOT NULL,
+    ledig BOOLEAN,
+	CONSTRAINT seteledigpådelstrekning_PK PRIMARY KEY (togreiseID, delstrekningsID, vognID, seteNR),
+	CONSTRAINT seteledigpådelstrekning_FK1 FOREIGN KEY (togreiseID) REFERENCES Togreise(togreiseID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT seteledigpådelstrekning_FK2 FOREIGN KEY (delstrekningsID) REFERENCES Delstrekning(delstrekningsID) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT seteledigpådelstrekning_FK3 FOREIGN KEY (vognID, seteNR) REFERENCES SeteIVogn(vognID, seteNR) ON UPDATE CASCADE ON DELETE CASCADE);
+	
+
+
+
+
+	
+
+	
+
+
+	
+
+
+
+
+
+
