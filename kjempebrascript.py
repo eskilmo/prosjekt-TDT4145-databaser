c078cc90fe849428ac2c331f392303849c437e45
Håkon <99hakon@hotmail.no>
2023-03-22 15:04:11 +0100

    kjempebrascript

--- kjempebrascript.py
+++ kjempebrascript.py
@@ -8,11 +8,33 @@
 
 
 # INSERT INTO SeteLedigPåDelstrekning (togreiseID, DelstrekningsID, vognID, seteNR, ledig) VALUES ()
-
+#togreiseID
 for i in range(1,7):
     if (i == 3 or i == 6):
+        #delstrekningsID
         for x in range(1,4):
-            print(f'''INSERT INTO SeteLedigPåDelstrekning (togreiseID, DelstrekningsID, vognID, seteNR, ledig) VALUES ({i}, {x}, {1}, {1}, {True})''')
+            #seteNR
+            for k in range(1, 13):
+                #vognID
+                if (i == 1 or i == 4):
+                    z = 1
+                    print(f'''INSERT INTO SeteLedigPåDelstrekning (togreiseID, DelstrekningsID, vognID, seteNR, ledig) VALUES ({i}, {x}, {z}, {k}, {True})''')
+                elif (i == 2 or i == 5):
+                    z = 4
+                elif (i == 3 or i == 6):
+                    z = 5
+                print(f'''INSERT INTO SeteLedigPåDelstrekning (togreiseID, DelstrekningsID, vognID, seteNR, ledig) VALUES ({i}, {x}, {z}, {k}, {True})''')
     else:
+        #delstrekningsID
         for j in range(1,6):
-            print(f'''INSERT INTO SeteLedigPåDelstrekning (togreiseID, DelstrekningsID, vognID, seteNR, ledig) VALUES ({i}, {j}, {1}, {1}, {True})''')
+            #seteNr
+            for h in range(1,13):
+                #vognID
+                if (i == 1 or i == 4):
+                    z = 1
+                    print(f'''INSERT INTO SeteLedigPåDelstrekning (togreiseID, DelstrekningsID, vognID, seteNR, ledig) VALUES ({i}, {j}, {z}, {h}, {True})''')
+                elif (i == 2 or i == 5):
+                    z = 4
+                elif (i == 3 or i == 6):
+                    z = 5
+                print(f'''INSERT INTO SeteLedigPåDelstrekning (togreiseID, DelstrekningsID, vognID, seteNR, ledig) VALUES ({i}, {j}, {z}, {h}, {True})''')
